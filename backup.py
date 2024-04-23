f70090d6a7f12ca49075d1f630573b26d90f76b7
BuildTools <rob.facella@gmail.com>
2024-04-23 17:36:49 -0400

    breaks on files names with a : in them

--- backup.py
+++ backup.py
@@ -25,5 +25,10 @@ print ( "Have you already run: ")
 print ( "rclone config")
 print ( " " )
 
-command = ( 'rclone ls ' + remoteDriveName + ':' )
+##!/bin/bash
+command = 'rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --stats-file-name-length 0 "' + remoteDriveName + ':" "' + childName + '/"'
+#
+#
+#
+# command = ( 'rclone ls ' + remoteDriveName + ':' )
 subprocess.Popen(command, shell=True)
