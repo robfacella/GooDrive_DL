0603fda6bcfeb32f27986dded851e871d4320549
BuildTools <rob.facella@gmail.com>
2024-04-23 17:07:12 -0400

    Config File for Remote Drive Name

--- backup.py
+++ backup.py
@@ -1,5 +1,15 @@
 
 import os
+import subprocess
+
+# Whatever you named your Remote when using: rclone config
+# Pull the REMOTE name
+file = open("remoteName.txt")
+rember = file.readlines()
+file.close()
+
+remoteDriveName=rember[0].strip("\n")
+print (remoteDriveName)
 
 print ("Hey, " + os.getlogin() + ", let's get started de-Googling your life.\n")
 
@@ -10,3 +20,10 @@ childName = childName.split("/")[-1]
 
 print ( "creating a subfolder for backup: " + childName )
 os.makedirs(childName, exist_ok=True)
+
+print ( "Have you already run: ")
+print ( "rclone config")
+print ( " " )
+
+command = ( 'rclone ls ' + remoteDriveName + ':' )
+subprocess.Popen(command, shell=True)
